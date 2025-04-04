09563900153e1cac7491134af9f4612d3224d239
peachjelly13 <ananyasharma112233441234@gmail.com>
2025-04-04 22:05:39 +0530

    to handle unhandled catch errors in promise

--- inject.js
+++ inject.js
@@ -2,7 +2,7 @@
     window.onerror = function(message,source,lineno,colno,error){
         //this is for logging errors, error received as an object
         sendError({
-            type:"JS_Error",
+            type:"javascript error",
             message,
             source,
             lineno,
@@ -10,6 +10,16 @@
             stack:error?.stack || "No stack trace",
         });
     };
+
+    window.onunhandledrejection = function(event){
+        sendError({
+            type:"unhandled rejection, this is for promise try and catch",
+            message:event.reason?.message || event.reason,
+            stack: event.error?.stack || "No stack trace"
+        })
+    };
+
+
 
 })();
 
