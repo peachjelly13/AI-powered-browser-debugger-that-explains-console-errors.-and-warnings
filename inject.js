d2f27b2fcd41e07817da62d99912888948cb472c
peachjelly13 <ananyasharma112233441234@gmail.com>
2025-04-06 00:59:16 +0530

    way to store errors,warnings

--- inject.js
+++ inject.js
@@ -20,10 +20,25 @@
     };
 
 
+    ["error","warn","log"].forEach((method)=>{
+        const method = console[method];
+        console[method] = function(...args){
+            sendError({
+                type:`Console:${method}`,
+                // we need to send all our error messages as strings
+                message:args.map(arg=>(typeof arg==='object'?JSON.stringify(arg):arg)).join(" ")
+            })
+            method.apply(console,args);
+        }
+    })
+
+
+
 
 })();
 
 //creating an iifie because we want to start reading errors as soon as we enter our web page
 //trying to majorly read three types of errors and warnings:
 // 1.errors
-// 2.
+// 2.promises that werent caught
+// 3.catching errors,warnings and logs
